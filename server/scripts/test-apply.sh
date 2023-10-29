9c139d5e0b1eeb4ad4befa5ce83d328952c21a1a
nopecho <ch.jooon@gmail.com>
2023-10-29 23:25:52 +0900

    chore

--- server/scripts/test-apply.sh
+++ server/scripts/test-apply.sh
@@ -18,28 +18,7 @@ function run_apply() {
     done
 }
 
-run_apply 7 &
-run_apply 7 &
-run_apply 7 &
-run_apply 7 &
-run_apply 7 &
-
 run_apply 1 &
-run_apply 2 &
-run_apply 3 &
-run_apply 4 &
-run_apply 5 &
-run_apply 6 &
-run_apply 7 &
-run_apply 8 &
-run_apply 9 &
-run_apply 10 &
-run_apply 16 &
-run_apply 93 &
-run_apply 2454 &
-run_apply 5592 &
-run_apply 2222 &
-run_apply 4444 &
 
 # Wait for all background processes to finish
 wait
